a10abbf05430c6e9447320e15835968df903bd8f
Salman Waheed <mkdirenv@gmail.com>
2020-09-22 11:54:59 +0400

    fix internal port

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((error, req, res, next) => {
 
 })
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
