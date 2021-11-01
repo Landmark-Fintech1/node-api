8fa2775781fe4c04d72f9e67cbb327eee70f5204
Salman Waheed <mkdirenv@gmail.com>
2021-11-01 18:25:08 +0500

    testing...

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-  res.json({ message: "Welcome to the API." })
+  res.json({ message: "DeletifyBot is deploying you via Slack." })
+  // res.json({ message: "Welcome to the API." })
 })
 
 const PORT = process.env.PORT || 9008
